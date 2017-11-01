08dea755114bc8573303ebc3dda491d844bc5125
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2017-11-01 18:50:46 +0200

    create new components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.template.html'
+})
+
+export class ServerComponent {
+  serverId = 10;
+
+}
