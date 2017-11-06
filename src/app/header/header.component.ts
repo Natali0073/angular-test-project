b066a088dbd90f4dd4de6d0c1b19e28210019dfa
Natali0073 <nataliamakarchuk17@gmail.com>
2017-11-06 23:13:08 +0200

    create components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.template.html'
+})
+
+export class HeaderComponent {
+  constructor () {
+
+  }
+}
