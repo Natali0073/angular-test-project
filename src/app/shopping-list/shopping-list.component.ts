c467f97d3fc237c519ddeaff0920bb813b765cd2
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2017-11-23 15:40:53 +0200

    uncommited changes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,21 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ShoppingListComponent implements OnInit {
 
   constructor() { }
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Orange', 1),
+    new Ingredient('Tomatos', 10),
+  ];
 
   ngOnInit() {
   }
